6f17d822c03a49c1e79392a810ed7914ffdeac64
limberg05 <hernandezdelriojuan@gmail.com>
2025-02-22 15:44:46 -0600

    arregle problemas visuales

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,24 +1,30 @@
 const Footer = () => {
   return (
-    <div className="bg-black h-52 text-white">
-      <div className=" grid grid-cols-2 py-14 text-2xl ">
-        <div className="flex justify-center space-x-4 mb-6">
-          <a href="/about" class="text-white">
-            Acerca de
-          </a>
-          <a href="/contact" class="text-white">
-            Contáctanos
-          </a>
-          <a href="/terms-of-service" class="text-white">
-            Términos
-          </a>
-          <a href="/privacy-policy" class="text-white">
-            Privacidad
-          </a>
-        </div>
-        <div>
-          &copy; 2025 Juan Limberg Hernandez del Rio. Todos los derechos
-          reservados.
+    <div className="bg-black text-white py-10">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {/* Sección de enlaces */}
+          <div className="flex flex-col md:flex-row md:justify-center md:space-x-6 mb-6 md:mb-0 text-center">
+            <a href="/about" className="hover:underline">
+              Acerca de
+            </a>
+            <a href="/contact" className="hover:underline">
+              Contáctanos
+            </a>
+            <a href="/terms-of-service" className="hover:underline">
+              Términos
+            </a>
+            <a href="/privacy-policy" className="hover:underline">
+              Privacidad
+            </a>
+          </div>
+
+          <div className="text-center md:text-left flex justify-center items-center">
+            <p>
+              &copy; 2025 Juan Limberg Hernandez del Rio. Todos los derechos
+              reservados.
+            </p>
+          </div>
         </div>
       </div>
     </div>
