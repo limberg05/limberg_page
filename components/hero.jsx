6e13b6587f699e7bdf569ac4e412a563fdb6a4a7
limberg05 <hernandezdelriojuan@gmail.com>
2025-02-23 05:57:38 -0600

    agregue una animacion de un coche, ya me quiero dormir

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,17 @@
+import '@/styles.css';
+
 const Hero = () => {
   return (
     <div>
       <h1 className="flex text-3xl justify-center sm:justify-start font-semibold  m-10 lg:text-4xl">
         Explora Mi Trabajo
       </h1>
+      <div className="coche">
+        <div className="rueda izquierda"></div>
+        <div className="rueda derecha"></div>
+        <div className="luces"></div>
+      </div>
+
       <p className="h-3px bg-gray-500 max-w-screen h-0.5 mb-11 m-10" />
 
       <div className="grid grid-cols-1 items-center sm:grid-cols-2 ">
@@ -17,7 +25,7 @@ const Hero = () => {
           <p className="flex font-semibold items-center justify-center bg-black text-white rounded-2xl m-4 h-14 p-4 w-56 sm:w-80 lg:h-20 text-xl sm:text-4xl">
             Eficiente
           </p>
-          <p className="flex font-semibold items-center justify-center bg-black text-white rounded-2xl m-4 h-14 p-4 w-56 sm:w-80 lg:h-20 text-xl sm:text-4xl">
+          <p className=" flex font-semibold items-center justify-center bg-black text-white rounded-2xl m-4 h-14 p-4 w-56 sm:w-80 lg:h-20 text-xl sm:text-4xl">
             Limberg
           </p>
         </div>
